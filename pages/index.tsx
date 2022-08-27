0bec5b091dc4e41c39b8ee67f36ac9c9b59b2c0f
finma <firmanmaulana2403@gmail.com>
2022-08-27 09:54:24 +0700

    [ADD] add index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
+import { Button, Divider } from "antd";
+import Link from "next/link";
 import type { NextPage } from "next";
+
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.card}>
+          <h1>StayUTBK</h1>
+          <Divider />
+          <div className={styles.space}>
+            <Link href="/login">
+              <Button block className="basic">
+                Login
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
