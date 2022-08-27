55257eba2d5a20af768d4a165f75fc1c055428f2
finma <firmanmaulana2403@gmail.com>
2022-08-27 20:11:20 +0700

    [ADD] add function to get layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
+import { ReactElement, ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import "antd/dist/antd.css";
 
+import "antd/dist/antd.css";
 import "../styles/globals.scss";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
     </QueryClientProvider>
