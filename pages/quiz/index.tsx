43b8bc5720992047d947bb8c463b2358bcde8cfc
finma <firmanmaulana2403@gmail.com>
2022-08-28 14:20:38 +0700

    [CHG] update button link

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -1,12 +1,25 @@
 import { ReactElement } from "react";
+import { Button } from "antd";
+import { useRouter } from "next/router";
+import cx from "classnames";
 
 import LayoutMain from "@/components/layout/layout-main";
 import PageTitle from "@/components/page-title";
 
 const QuizPage = () => {
+  const router = useRouter();
   return (
     <>
-      <PageTitle title="Quiz" breadcrumbs="Home - Quiz" />
+      <section className="page-header">
+        <PageTitle title="Quiz" breadcrumbs="Home - Quiz" />
+
+        <Button
+          onClick={() => router.push("/quiz/create")}
+          className={cx("button", "success")}
+        >
+          Add Quiz
+        </Button>
+      </section>
     </>
   );
 };
