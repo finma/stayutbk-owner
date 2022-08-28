8ac727b396ba826df908907a0d3cda72d3e06db4
finma <firmanmaulana2403@gmail.com>
2022-08-28 13:52:21 +0700

    [ADD] add table component

--- pages/users/roles.tsx
+++ pages/users/roles.tsx
@@ -1,11 +1,41 @@
 import { ReactElement } from "react";
-import { Button } from "antd";
+import { Button, Table } from "antd";
 import cx from "classnames";
+import type { ColumnsType } from "antd/lib/table";
 
 import LayoutMain from "@/components/layout/layout-main";
 import PageTitle from "@/components/page-title";
 
+interface DataType {
+  roleName: string;
+}
+
+const dataTable = [
+  { id: 1, roleName: "Super Admin" },
+  { id: 2, roleName: "Admin" },
+  { id: 3, roleName: "User" },
+  { id: 4, roleName: "Student" },
+];
+
 const RolesPage = () => {
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "#",
+      dataIndex: "index",
+      key: "index",
+      width: "30px",
+      render: (text, record, index) => {
+        return <span>{index + 1}</span>;
+      },
+    },
+    {
+      title: "Role Name",
+      dataIndex: "roleName",
+      key: "roleName",
+      sorter: (a, b) => (a?.roleName ?? "").localeCompare(b?.roleName ?? ""),
+    },
+  ];
+
   return (
     <>
       <section className="page-header">
@@ -16,6 +46,10 @@ const RolesPage = () => {
         >
           Add Role
         </Button>
+      </section>
+
+      <section style={{ marginTop: 24 }}>
+        <Table columns={columns} dataSource={dataTable} pagination={false} />
       </section>
     </>
   );
