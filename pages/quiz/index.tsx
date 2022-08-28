752099967b92ed1fed35160732f0da9c916154af
finma <firmanmaulana2403@gmail.com>
2022-08-28 14:37:45 +0700

    [ADD] add table quiz

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -1,13 +1,47 @@
 import { ReactElement } from "react";
-import { Button } from "antd";
+import { Button, Table } from "antd";
 import { useRouter } from "next/router";
 import cx from "classnames";
+import type { ColumnsType } from "antd/lib/table";
 
 import LayoutMain from "@/components/layout/layout-main";
 import PageTitle from "@/components/page-title";
 
+interface DataType {
+  quiz: string;
+  totalQuestion: number;
+  totalTime: number;
+}
+
+const dataTable = [
+  { id: 1, quiz: "Quiz 1", totalQuestion: 10, totalTime: 60 },
+  { id: 2, quiz: "Quiz 2", totalQuestion: 10, totalTime: 60 },
+  { id: 3, quiz: "Quiz 3", totalQuestion: 10, totalTime: 60 },
+];
+
 const QuizPage = () => {
   const router = useRouter();
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "Quiz",
+      dataIndex: "quiz",
+      key: "quiz",
+    },
+    {
+      title: "Total Question",
+      dataIndex: "totalQuestion",
+      key: "totalQuestion",
+      width: "200px",
+    },
+    {
+      title: "Total Time (minute)",
+      dataIndex: "totalTime",
+      key: "totalTime",
+      width: "200px",
+    },
+  ];
+
   return (
     <>
       <section className="page-header">
@@ -19,6 +53,10 @@ const QuizPage = () => {
         >
           Add Quiz
         </Button>
+      </section>
+
+      <section style={{ marginTop: 24 }}>
+        <Table columns={columns} dataSource={dataTable} />
       </section>
     </>
   );
